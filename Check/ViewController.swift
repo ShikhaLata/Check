27bfdfba2ffcca770863c05f55d2db10167766e6
ShikhaLata <shikha1234lata@gmail.com>
2018-01-22 00:03:01 +1100

    Initial Commit

---
+++ Check/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Check
+//
+//  Created by Shikha Lata on 22/1/18.
+//  Copyright © 2018 Shikha Lata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
